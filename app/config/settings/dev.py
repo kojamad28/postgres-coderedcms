ee38b5797fe026b575fdcebc76b6196662611676
kojamad28 <kojamad28@gmail.com>
2025-05-04 06:41:32 +0900

    created the coderedcms scripts

---
+++ app/config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ")fv^f^14t8+8l@s6*mh3=qs^8cu1w32fuj%hi-ldlm6cc(vejr"
+
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local import *  # noqa
+except ImportError:
+    pass
